377dcb0afdfcc2a362009d1e02c7deb65ee67772
Djordje Lukic <djordje.lukic@docker.com>
2022-12-16 12:09:09 +0100

    Remove unnecessary mounts
    OCI mounts are already done in task_create
    
    Signed-off-by: Djordje Lukic <djordje.lukic@docker.com>

--- crates/containerd-shim-wasm/src/sandbox/shim.rs
+++ crates/containerd-shim-wasm/src/sandbox/shim.rs
@@ -1,7 +1,6 @@
 use std::collections::HashMap;
 use std::env::current_dir;
 use std::fs::{self, File};
-use std::fs::{canonicalize, create_dir_all, OpenOptions};
 use std::ops::Not;
 use std::os::unix::io::AsRawFd;
 use std::path::Path;
@@ -30,7 +29,7 @@ use nix::mount::{mount, MsFlags};
 use nix::sched::{setns, unshare, CloneFlags};
 use nix::sys::stat::Mode;
 use nix::unistd::mkdir;
-use oci_spec::runtime::{self, Mount};
+use oci_spec::runtime;
 use ttrpc::context::Context;
 
 use super::instance::{EngineGetter, Instance, InstanceConfig, Nop};
@@ -1354,42 +1353,6 @@ where
         )
         .map_err(|err| shim::Error::Other(format!("failed to remount rootfs as slave: {}", err)))?;
 
-        if let Some(mounts) = spec.mounts() {
-            for m in mounts {
-                if m.typ().as_ref().unwrap() != "bind" {
-                    continue;
-                }
-
-                let dest = m.destination();
-                let source = m.source().as_ref().unwrap();
-                let src = canonicalize(source).unwrap();
-
-                let dir = if src.is_file() {
-                    Path::new(&dest).parent().unwrap()
-                } else {
-                    Path::new(&dest)
-                };
-
-                create_dir_all(dir).unwrap();
-
-                if src.is_file() {
-                    OpenOptions::new()
-                        .create(true)
-                        .write(true)
-                        .open(&dest)
-                        .unwrap();
-                }
-
-                mount::<str, Path, str, str>(
-                    Some(&src.to_string_lossy()),
-                    dest,
-                    None,
-                    parse_mount(m),
-                    None,
-                )?;
-            }
-        }
-
         let (_child, address) = shim::spawn(opts, &grouping, envs)?;
 
         write_address(&address)?;
@@ -1415,57 +1378,6 @@ where
             ..Default::default()
         })
     }
-}
-
-pub fn parse_mount(m: &Mount) -> MsFlags {
-    let mut flags = MsFlags::empty();
-    if let Some(options) = &m.options() {
-        for s in options {
-            if let Some((is_clear, flag)) = match s.as_str() {
-                "defaults" => Some((false, MsFlags::empty())),
-                "ro" => Some((false, MsFlags::MS_RDONLY)),
-                "rw" => Some((true, MsFlags::MS_RDONLY)),
-                "suid" => Some((true, MsFlags::MS_NOSUID)),
-                "nosuid" => Some((false, MsFlags::MS_NOSUID)),
-                "dev" => Some((true, MsFlags::MS_NODEV)),
-                "nodev" => Some((false, MsFlags::MS_NODEV)),
-                "exec" => Some((true, MsFlags::MS_NOEXEC)),
-                "noexec" => Some((false, MsFlags::MS_NOEXEC)),
-                "sync" => Some((false, MsFlags::MS_SYNCHRONOUS)),
-                "async" => Some((true, MsFlags::MS_SYNCHRONOUS)),
-                "dirsync" => Some((false, MsFlags::MS_DIRSYNC)),
-                "remount" => Some((false, MsFlags::MS_REMOUNT)),
-                "mand" => Some((false, MsFlags::MS_MANDLOCK)),
-                "nomand" => Some((true, MsFlags::MS_MANDLOCK)),
-                "atime" => Some((true, MsFlags::MS_NOATIME)),
-                "noatime" => Some((false, MsFlags::MS_NOATIME)),
-                "diratime" => Some((true, MsFlags::MS_NODIRATIME)),
-                "nodiratime" => Some((false, MsFlags::MS_NODIRATIME)),
-                "bind" => Some((false, MsFlags::MS_BIND)),
-                "rbind" => Some((false, MsFlags::MS_BIND | MsFlags::MS_REC)),
-                "unbindable" => Some((false, MsFlags::MS_UNBINDABLE)),
-                "runbindable" => Some((false, MsFlags::MS_UNBINDABLE | MsFlags::MS_REC)),
-                "private" => Some((true, MsFlags::MS_PRIVATE)),
-                "rprivate" => Some((true, MsFlags::MS_PRIVATE | MsFlags::MS_REC)),
-                "shared" => Some((true, MsFlags::MS_SHARED)),
-                "rshared" => Some((true, MsFlags::MS_SHARED | MsFlags::MS_REC)),
-                "slave" => Some((true, MsFlags::MS_SLAVE)),
-                "rslave" => Some((true, MsFlags::MS_SLAVE | MsFlags::MS_REC)),
-                "relatime" => Some((true, MsFlags::MS_RELATIME)),
-                "norelatime" => Some((true, MsFlags::MS_RELATIME)),
-                "strictatime" => Some((true, MsFlags::MS_STRICTATIME)),
-                "nostrictatime" => Some((true, MsFlags::MS_STRICTATIME)),
-                _ => None,
-            } {
-                if is_clear {
-                    flags &= !flag;
-                } else {
-                    flags |= flag;
-                }
-            };
-        }
-    }
-    flags
 }
 
 fn forward_events(
